30dd2873b517afa600947ff2cbca3b25aa9c4a6e
Shinya <Shinya.Tanaka1@jp.ricoh.com>
2020-11-19 17:01:47 +0900

    [add] upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDom from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+interface SquarePropsInterface {
+  //value: String;
 }
 
-export default App;
+interface SquareStateInterface {
+  value: string;
+}
+
+class MovieForm extends React.Component<SquarePropsInterface, SquareStateInterface> {
+  constructor(props:SquarePropsInterface) {
+    super(props);
+    this.state = {value:''}
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange = (event:any) => {
+    this.setState({value: event.target.value});
+    
+  }
+  handleSubmit = (event:any) => {
+    
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="file" accept = "video/mp4" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default MovieForm;
+
