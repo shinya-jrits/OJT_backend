194125c4d962aff6f2ea48a57277934087182b6f
shinya_t <shinya.tanaka1@jp.ricoh.com>
2020-12-03 16:35:17 +0900

    multerを使ってpostされたファイルを受け取って保存する

--- express-nodejs/index.ts
+++ express-nodejs/index.ts
@@ -1,19 +1,31 @@
 import express from 'express'
 
-const app: express.Express = express()
+const multer = require('multer');
+const app: express.Express = express();
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "origin, X-Requested-With, Content-Type, Accept")
-    next()
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "origin, X-Requested-With, Content-Type, Accept");
+    next();
 })
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const router: express.Router = express.Router()
+const router: express.Router = express.Router();
 router.get('/api/getTest', (req: express.Request, res: express.Response) => {
-    res.send(req.query)
-})
+    res.send(req.query);
+    console.log(req.query);
+});
 
-router.post('/api/po')
+const upload = multer({ dest: './audioFile' });
+const cpupload = upload.fields([{ name: 'upfile', maxCount: 1 }, { name: 'name', maxCount: 1 }]);
+router.post('/api/postTest', cpupload, (req: express.Request, res: express.Response) => {
+    res.send(req.body);
+    console.log(req.body.mail);
+    //@ts-ignore
+    console.log(req.files['upfile']);
+});
+app.use(router);
+
+app.listen(3000, () => { console.log('example app listening on port 3000!') });
