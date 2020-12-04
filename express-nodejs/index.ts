0d7a006b5bfc751c388bb6f1033a5fc11a97e1e6
shinya_t <shinya.tanaka1@jp.ricoh.com>
2020-12-04 16:53:47 +0900

    GCSへファイルをアップロードする。(ディレクトリーのファイルを）

--- express-nodejs/index.ts
+++ express-nodejs/index.ts
@@ -1,6 +1,11 @@
 import express from 'express'
+import { Storage } from '@google-cloud/storage'
 
 const multer = require('multer');
+
+const google_auth_info = require('./node-js-test-292505-c768dadc8230.json');
+
+
 const app: express.Express = express();
 
 app.use((req, res, next) => {
@@ -12,10 +17,29 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const storage = new Storage({
+    projectId: "node-js-test-292505",
+    keyFilename: "./node-js-test-292505-c768dadc8230.json"
+});
+
+async function uploadFile() {
+    await storage.bucket('example_backet').upload('./audioFile/testAudio.wav', {
+        gzip: true,
+        metadata: {
+            cacheControl: 'public, max-age=31536000',
+        },
+    });
+
+    console.log('uplaoded file');
+
+    uploadFile().catch(console.error);
+}
+
 const router: express.Router = express.Router();
 router.get('/api/getTest', (req: express.Request, res: express.Response) => {
     res.send(req.query);
     console.log(req.query);
+    uploadFile();
 });
 
 const upload = multer({ inMemory: true });
