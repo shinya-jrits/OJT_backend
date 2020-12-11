7056809231e077eddb8ebc7e270deab2cdf79080
shinya_t <shinya.tanaka1@jp.ricoh.com>
2020-12-11 17:13:51 +0900

    [add] axiosでのpost追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 import './App.css';
+import axios from 'axios';
 
 interface convertVideoToAudioStateInterface {
   videoFile: File;
+  EmailAddress: string;
 }
 
 class MovieForm extends React.Component<{}, convertVideoToAudioStateInterface> {
@@ -31,19 +33,47 @@ class MovieForm extends React.Component<{}, convertVideoToAudioStateInterface> {
         });
       })
     }
+    if (event.target.value != null) {
+      this.setState({
+        EmailAddress: event.target.value,
+      });
+    }
   }
   handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
 
     const result = this.convertVideoToAudio(this.state.videoFile);
+
+    result.then((file) => {
+      //postリクエスト
+      console.log("post sending ")
+      const params = new FormData();
+      params.append('mail', this.state.EmailAddress);
+      params.append('file', file);
+
+      axios.post("http://localhost:4000/api/", params, {
+        headers: {
+          'content-type': 'multipart/form-data',
+        },
+      })
+        .then(function (responese) {
+          console.log("post request success");
+          window.alert("送信に成功しました");
+        })
+        .then(function (error) {
+          console.log(console.error);
+          window.alert("送信に失敗しました")
+        });
+    });
+
     //リリース前には削除予定
-    result.then((result) => {
+    /*result.then((result) => {
       const url = window.URL.createObjectURL(new Blob([result]));
       const anchor = document.createElement('a');
       anchor.href = url;
       anchor.setAttribute('download', 'audio.wav');
       anchor.click();
 
-    })
+    })*/
     event.preventDefault();//ページ遷移を防ぐため
   }
   render() {
@@ -51,8 +81,8 @@ class MovieForm extends React.Component<{}, convertVideoToAudioStateInterface> {
       <div>
         <form onSubmit={this.handleSubmit}>
           <label>
-            Name:
-          <input type="file" accept="video/mp4" onChange={this.handleChange} />
+            <p>メールアドレス:<input type="email" name="mail" onChange={this.handleChange} /></p>
+            <p>ファイル:<input type="file" accept="video/mp4" onChange={this.handleChange} /></p>
           </label>
           <input type="submit" value="Submit" />
         </form>
