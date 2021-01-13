5c02e29601aeae27f9e8e8d893d1c1a671896bb0
shinya09 <74222581+shinya09@users.noreply.github.com>
2021-01-13 13:52:05 +0900

    [update] if文について早期リターンに変更する

--- express-nodejs/src/index.ts
+++ express-nodejs/src/index.ts
@@ -84,11 +84,13 @@ async function getSecretApiKey(secretId: string): Promise<string | null> {
     const [accessResponse] = await client.accessSecretVersion({
         name: 'projects/483600820879/secrets/' + secretId + '/versions/latest',
     })
-    if (accessResponse.payload?.data != null) {
+    if (accessResponse.payload?.data == null) {
+        return null;
+    } else {
         const responsePayload = accessResponse.payload.data.toString();
         return responsePayload;
     }
-    return null;
+
 }
 
 async function speechToText(fileName: string, address: string) {
