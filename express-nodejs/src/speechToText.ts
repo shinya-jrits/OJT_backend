a7498e45c8a0642a37fbcbb60ef10cfd6dc05017
shinya09 <74222581+shinya09@users.noreply.github.com>
2021-02-01 14:24:39 +0900

    [update] AB#1106 speechToTextを別ファイルに保存する

---
+++ express-nodejs/src/speechToText.ts
@@ -0,0 +1,32 @@
+import Speech from '@google-cloud/speech'
+
+export async function speechToText(fileName: string, bucketName?: string): Promise<string | null> {
+    const client = new Speech.v1p1beta1.SpeechClient();
+    const config = {
+        languageCode: 'ja-JP',
+        enableAutomaticPunctuation: true,
+    };
+    const audio = {
+        uri: 'gs://' + bucketName + '/' + fileName,
+    };
+    const request = {
+        config: config,
+        audio: audio,
+    };
+
+    const [operation] = await client.longRunningRecognize(request);
+
+    const [responese] = await operation.promise();
+    if (responese.results == null) {
+        console.error("文字起こしに失敗しました");
+        return null;
+    } else if (responese.results.length === 0) {
+        console.log("文字を検出できませんでした。");
+        return null;
+    } else {
+        console.log("文字起こしが完了しました");
+        return responese.results
+            .filter(resutlt => resutlt.alternatives != null)
+            .map(result => result.alternatives![0].transcript).join('\n');
+    }
+}
