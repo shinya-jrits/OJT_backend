590c0377a45b2ff734d287731b0d3591c4fe58e3
shinya09 <74222581+shinya09@users.noreply.github.com>
2021-02-02 15:45:43 +0900

    [update] AB#1115 getSecretMangerValueをgetSecretValueに変更する

--- express-nodejs/src/SecretManager.ts
+++ express-nodejs/src/SecretManager.ts
@@ -4,7 +4,7 @@ import { SecretManagerServiceClient } from '@google-cloud/secret-manager'
  * SecretManagerからシークレット値を取り出す
  * @param secretId シークレットの名前
  */
-export async function getSecretManagerValue(secretId: string): Promise<string | null> {
+export async function getSecretValue(secretId: string): Promise<string | null> {
     const client = new SecretManagerServiceClient();
     const [accessResponse] = await client.accessSecretVersion({
         name: 'projects/483600820879/secrets/' + secretId + '/versions/latest',
